35114b718a7e2ddea41c9853c261579451b1d479
luzhenpeng <zhenpenglu@gmail.com>
2021-11-28 14:54:24 +0800

    2-2dweight.c

--- 2-2dweight.c
+++ 2-2dweight.c
@@ -2,3 +2,21 @@
 // Created by 卢振鹏 on 2021/11/28.
 //
 
+#include "stdio.h"
+
+int main(void)
+{
+    int height, length, width, volume, weight;
+
+    height = 8;
+    length = 12;
+    width = 10;
+    volume = height * length * width;
+    weight = (volume + 165) / 166;
+
+    printf("Dimensions: %dx%dx%d\n", length, width, height);
+    printf("Volume (cubic inches): %d\n",volume);
+    printf("Dimensional weight (pounds): %d\n", weight);
+
+    return 0;
+}
