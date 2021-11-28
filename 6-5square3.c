b907452b80fd99ffdec02b891fac3eef7f760877
luzhenpeng <zhenpenglu@gmail.com>
2021-11-28 22:01:20 +0800

    202111282201

---
+++ 6-5square3.c
@@ -0,0 +1,24 @@
+//
+// Created by 卢振鹏 on 2021/11/28.
+//
+
+#include "stdio.h"
+
+int main(void)
+{
+    int i, n, odd, square;
+
+    printf("This program prints a table of squares.\n");
+    printf("Enter number of entries in table: ");
+    scanf("%d", &n);
+
+    i = 1;
+    odd = 3;
+    for (square = 1; i <= n ; odd += 2) {
+        printf("%10d%10d\n", i, square);
+        ++i;
+        square += odd;
+    }
+
+    return 0;
+}
